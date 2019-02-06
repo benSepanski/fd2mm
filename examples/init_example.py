7b8849228816b23a555a7348a5ef64df5411e3a2
Ben Sepanski <ben_sepanski@baylor.edu>
2019-02-06 14:13:34 -0600

    Renamed tests more appropriately as examples. Fixed init_example, others still need to be fixed

--- examples/init_example.py
+++ examples/init_example.py
@@ -4,17 +4,17 @@ PYTHONPATH=$PYTHONPATH:<parent directory of whatever folder the package is in>
 """
 
 import firedrake as fd
-from firedrake_to_pytential import FiredrakeToMeshmodeConverter
+from firedrake_to_pytential import FiredrakeMeshmodeConnection
 
 m = fd.Mesh('square_ring.msh')
 m.init()
 V = fd.FunctionSpace(m, 'DG', 1)
-converter = FiredrakeToMeshmodeConverter(V, ambient_dim=2)
-py_mesh = converter.get_meshmode_mesh()
+converter = FiredrakeMeshmodeConnection(V, ambient_dim=2)
+py_mesh = converter.meshmode_mesh
 
 xx = fd.SpatialCoordinate(m)
 f = fd.Function(V).interpolate(fd.sin(xx[0]))
-target = converter.fd_function_to_array(f)
+target = converter(None, f)
 
 from meshmode.mesh import check_bc_coverage
-check_bc_coverage(py_mesh, [1,2])
+check_bc_coverage(py_mesh, [1, 2])
