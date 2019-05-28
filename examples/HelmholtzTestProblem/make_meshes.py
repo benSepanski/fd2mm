be617a2e7758d92f097e0799dbb5285756de73cc
Ben Sepanski <ben_sepanski@baylor.edu>
2019-05-28 14:09:20 -0500

    Stopped storing data files

--- examples/HelmholtzTestProblem/make_meshes.py
+++ examples/HelmholtzTestProblem/make_meshes.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 from subprocess import call
 import math
+import os
 
 mesh_file = "domain"
 ext = ".geo"
 out_folder = "msh_files/"
-num_refine = 8
+num_refine = 7
 const = 2.0
 
-clmax = [pow(const, -n) for n in range(6, num_refine)]
+clmax = [pow(const, -n) for n in range(num_refine)]
 clmin = [m / 2 for m in clmax]
 
 for mx, mn in zip(clmax, clmin):
@@ -17,6 +18,9 @@ for mx, mn in zip(clmax, clmin):
     out_name = out_name.replace('.', '%')
     out_name += '.msh'
 
-    call(["gmsh", "-2",
-          "-clmax", str(mx), "-clmin", str(mn),
-          mesh_file + ext, "-o", out_name])
+    # if not already instantiated
+    if not os.path.isfile(out_name):
+
+        call(["gmsh", "-2",
+              "-clmax", str(mx), "-clmin", str(mn),
+              mesh_file + ext, "-o", out_name])
